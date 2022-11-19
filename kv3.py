f3d5c45e52d3d88faed59b4ff1b8f10a5cd910e4
kristiker <kristik06@outlook.com>
2022-11-19 18:02:08 +0100

    Test and type hint all KV3File ctor overloads

--- kv3.py
+++ kv3.py
@@ -1,9 +1,10 @@
+import dataclasses
 from pathlib import Path
-from dataclasses import asdict, dataclass, is_dataclass
+from typing import Iterable, Protocol, Sequence, overload
 from uuid import UUID
 import enum
 
-@dataclass(frozen=True)
+@dataclasses.dataclass(frozen=True)
 class KV3Header:
     encoding: str = 'text'
     encoding_ver: str = 'e21c7f3c-8a33-41c5-9977-a76d3a32aa0d'
@@ -33,7 +34,7 @@ class flag(enum.Flag):
     def __str__(self):
         return "+".join(val.name for val in self.__class__ if self.value & val)
 
-@dataclass(slots=True)
+@dataclasses.dataclass(slots=True)
 class flagged_value:
     value: value_types
     flags: flag = flag(0)
@@ -44,11 +45,32 @@ class flagged_value:
 
 value_types = value_types | flagged_value
 
+class Dataclass(Protocol):
+    # checking for this attribute is currently
+    # the most reliable way to ascertain that something is a dataclass
+    __dataclass_fields__: dict[str, dataclasses.Field]
+
 class KV3File(dict):
+    @overload
+    def __init__(self):
+        """Create an empty dict-based KV3File""" # TODO: null?
+        ...
+    @overload
+    def __init__(self, **keywordargs):
+        """Create a dict-based KV3File from keyword arguments"""
+        ...
+    @overload
+    def __init__(self, kv3_dataclass: Dataclass):
+        """Create a dict-based KV3File from a dataclass"""
+        ...
+    @overload
+    def __init__(self, keyvaluepairs: Iterable[tuple[str, value_types]]):
+        """Create a dict-based KV3File from a list of key-value pairs"""
+        ...
+
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if len(args) and is_dataclass(args[0]):
-            super().__init__(asdict(args[0]))
+        if len(args) and dataclasses.is_dataclass(args[0]):
+            super().__init__(dataclasses.asdict(args[0]))
         else:
             super().__init__(*args, **kwargs)
         self.header = KV3Header()
@@ -116,19 +138,58 @@ if __name__ == '__main__':
             header = KV3Header('text2', '123-123', 'generic2', '234-234')
             headertext = '<!-- kv3 encoding:text2:version{123-123} format:generic2:version{234-234} -->'
             self.assertEqual(str(header), headertext)
+        
+        def test_empty_instantiated_kv3file(self):
+            self.assertEqual(
+                KV3File().ToString(),
+                self.default_header + "\n{\n}"
+            )
 
-        def test_kv3file_dict(self):
-            expect_text = f'{self.default_header}\n{{'+\
+        def test_kwargs_instantiated_kv3file(self):
+            expect_text = self.default_header + '\n{'+\
                 '\n\ta = "asd asd"'+\
-                '\n\tb = \n\t{\n\t\t"2" = 3\n\t}'+\
+                '\n\tb = \n\t{\n\t\tinner_b = 3\n\t}'+\
                 '\n\tc = ["listed_text1", "listed_text2"]\n}'
             self.assertEqual(
                 KV3File(
                     a='asd asd',
-                    b={2:3},
+                    b={"inner_b":3},
                     c=["listed_text1", "listed_text2"]
                 ).ToString(),
                 expect_text
+            )
+
+        def test_dataclass_instantiated_kv3file(self):
+            @dataclasses.dataclass
+            class MyKV3Format:
+                a: str = 'asd asd'
+                b: dict = dataclasses.field(default_factory=lambda: {"inner_b":3})
+            self.assertEqual(
+                KV3File(MyKV3Format()).ToString(),
+                self.default_header + '\n{'+\
+                    '\n\ta = "asd asd"'+\
+                    '\n\tb = \n\t{\n\t\tinner_b = 3\n\t}\n}'
+            )
+        
+        def test_dict_instantiated_kv3file(self):
+            self.assertEqual(
+                KV3File({
+                    'a': 'asd asd',
+                    'b': {"inner_b":3},
+                    'c': ["listed_text1", "listed_text2"]
+                }).ToString(),
+                self.default_header + '\n{'+\
+                    '\n\ta = "asd asd"'+\
+                    '\n\tb = \n\t{\n\t\tinner_b = 3\n\t}'+\
+                    '\n\tc = ["listed_text1", "listed_text2"]\n}'
+            )
+        
+        def test_keyvaluepairs_kv3file(self):
+            self.assertEqual(
+                KV3File([('a', 'asd asd'), ('b', {'inner_b':3})]).ToString(),
+                self.default_header + '\n{'+\
+                    '\n\ta = "asd asd"'+\
+                    '\n\tb = \n\t{\n\t\tinner_b = 3\n\t}\n}'
             )
 
     unittest.main()
