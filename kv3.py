bdc49945497985da9dae09268a9e61848a035cf1
kristiker <kristik06@outlook.com>
2022-11-19 17:28:10 +0100

    Switch if/elif to pattern matching

--- kv3.py
+++ kv3.py
@@ -51,41 +51,48 @@ class KV3File(dict):
 
     def __str__(self):
         kv3 = str(self.header) + '\n'
-
         def obj_serialize(obj, indent = 1, dictKey = False):
             preind = ('\t' * (indent-1))
             ind = ('\t' * indent)
-            if obj is None:
-                return 'null'
-            elif isinstance(obj, bool):
-                if obj: return 'true'
-                return 'false'
-            elif isinstance(obj, str):
-                return '"' + obj + '"'
-            elif isinstance(obj, list):
-                s = '['
-                if any(isinstance(item, dict) for item in obj):  # TODO: only non numbers
-                    s = f'\n{preind}[\n'
-                    for item in obj:
-                        s += (obj_serialize(item, indent+1) + ',\n')
-                    return s + preind + ']\n'
+            match obj:
+                case flagged_value(value, flags):
+                    if flags:
+                        return f"{flags}:{obj_serialize(value)}"
+                    return obj_serialize(value)
+                case None:
+                    return 'null'
+                case False:
+                    return 'false'
+                case True:
+                    return 'true'
+                case str():
+                    return '"' + obj + '"'
+                case str_multiline():
+                    return '"""' + obj + '"""'
+                case list():
+                    s = '['
+                    if any(isinstance(item, dict) for item in obj):  # TODO: only non numbers
+                        s = f'\n{preind}[\n'
+                        for item in obj:
+                            s += (obj_serialize(item, indent+1) + ',\n')
+                        return s + preind + ']\n'
 
-                return f'[{", ".join((obj_serialize(item, indent+1) for item in obj))}]'
-            elif isinstance(obj, dict):
-                s = preind + '{\n'
-                if dictKey:
-                    s = '\n' + s
-                for key, value in obj.items():
-                    #if value == [] or value == "" or value == {}: continue
-                    if not isinstance(key, str):
-                        key = f'"{key}"'
-                    s +=  ind + f"{key} = {obj_serialize(value, indent+1, dictKey=True)}\n"
-                return s + preind + '}'
-            else: # int, float, resource
-                # round off inaccurate dmx floats
-                if type(obj) == float:
-                    obj = round(obj, 6)
-                return str(obj)
+                    return f'[{", ".join((obj_serialize(item, indent+1) for item in obj))}]'
+                case dict():
+                    s = preind + '{\n'
+                    if dictKey:
+                        s = '\n' + s
+                    for key, value in obj.items():
+                        #if value == [] or value == "" or value == {}: continue
+                        if not isinstance(key, str):
+                            key = f'"{key}"'
+                        s +=  ind + f"{key} = {obj_serialize(value, indent+1, dictKey=True)}\n"
+                    return s + preind + '}'
+                case _: # int, float, resource
+                    # round off inaccurate dmx floats
+                    if type(obj) == float:
+                        obj = round(obj, 6)
+                    return str(obj)
 
         kv3 += obj_serialize(self)
 
